d2d407b240a0e2da5f61b097beb4beee4b498c2b
zell92 <n.sardella@hotmail.it>
2016-05-28 18:08:38 +0200

    Put per carmaker

--- src/it/uniroma3/resurce/CarmakerResurce.java
+++ src/it/uniroma3/resurce/CarmakerResurce.java
@@ -1,17 +1,20 @@
 package it.uniroma3.resurce;
 
 
+import java.util.LinkedList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+
+import it.uniroma3.resurce.Carmaker;
 import it.uniroma3.resurce.Car;
 
 
 @Stateless
-@Path("/carmaker/{id}")
+@Path("/carmaker")
 public class CarmakerResurce {
 
 	@Context
@@ -19,7 +22,7 @@ public class CarmakerResurce {
 
 	@PersistenceContext(unitName = "unit-progetto")
 	private EntityManager em;
-
+	@Path("/{id}")
 	@GET
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Carmaker getCarmaker(@PathParam("id") int id) {
@@ -38,10 +41,9 @@ public class CarmakerResurce {
 		}
 	}
 
-
+	@Path("/{id}")
 	@DELETE
-	//@Transactional
-	public Response deleteProduct(@PathParam("id") int id) {
+	public Response deleteCarmaker(@PathParam("id") int id) {
 		try {
 			Carmaker carmaker = em.find(Carmaker.class, id);
 			em.remove(carmaker);
@@ -53,7 +55,24 @@ public class CarmakerResurce {
 					.entity(errorMessage).type("text/plain").build());
 		}
 	}
-
+	
+	@Path("/{name}")
+	@PUT
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response insertCarmaker(@PathParam("name") String name) {
+		try {
+			List<Car> cars = new LinkedList<Car>();
+			Carmaker carmaker = new Carmaker(name, cars);
+			em.persist(carmaker);
+			
+			return Response.ok(carmaker).status(Response.Status.OK).build();
+		} catch (Exception e) {
+			String errorMessage = "Error";
+			throw new WebApplicationException(
+					Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+					.entity(errorMessage).type("text/plain").build());
+		}
+	}
 
 
 }
